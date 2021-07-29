ebffcba87249f6471a32aa6d3352ca61aff3245e
suppolyar <vkhardy@inbox.ru>
2021-07-29 19:20:32 +0300

    v 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div class="app">
+        <post-form
+            @create="createPost"
+        />
+        <post-list 
+            :posts="posts"
+        />        
+    </div>
 </template>
 
+<script>
+import PostForm from './conponents/PostForm.vue'
+import PostList from './conponents/PostList.vue'
+import PostItem from './conponents/PostItem.vue'
+export default {
+    components: {
+        PostForm, PostList, PostItem
+    },
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание 3'}
+            ],
+        }
+    },
+    methods: {
+        createPost(post) {
+            this.posts.push(post);
+        },
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 </style>
